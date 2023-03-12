6ee5b6bc8eb0f635c6ea8ad012bd0c7b9797286d
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-03-12 14:42:36 +0700

    feat: event

--- app/services/mongoose/images.js
+++ app/services/mongoose/images.js
@@ -1,17 +1,17 @@
 const Images = require("../../api/v1/images/model");
+const { NotFoundError } = require("../../errors/index");
 
-// ada 2 cara 
-// 1. gunakan cara ini 
+// ada 2 cara
+// 1. gunakan cara ini
 // 2. gunakan generate url images
 
-
 // 2. gunakan generate url images
-const generateUrlImages = async(req) => {
-  const result = `uploads/${req.file.filename}`;
-  return result;
-}
+// const generateUrlImages = async(req) => {
+//   const result = `uploads/${req.file.filename}`;
+//   return result;
+// }
 
-// 1. gunakan cara ini 
+// 1. gunakan cara ini
 const createImages = async (req) => {
   const result = await Images.create({
     name: req.file
@@ -21,4 +21,14 @@ const createImages = async (req) => {
   return result;
 };
 
-module.exports = { createImages, generateUrlImages };
+// tambahkan function checking Image
+const checkingImage = async (id) => {
+  const result = await Images.findOne({ _id: id });
+  console.log(result);
+
+  if (!result) throw new NotFoundError(`Tidak ada Gambar dengan id :  ${id}`);
+
+  return result;
+};
+
+module.exports = { createImages, checkingImage };
