5635f94319749b792e316984d69d411fe3741e8c
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-03-16 20:59:00 +0700

    feat: order fix

--- app/api/v1/events/controller.js
+++ app/api/v1/events/controller.js
@@ -4,27 +4,29 @@ const {
   updateEvents,
   createEvents,
   deleteEvents,
+  changeStatusEvents
 } = require("../../../services/mongoose/events");
 
 const { StatusCodes } = require("http-status-codes");
-
-const create = async (req, res, next) => {
-  try {
-    const result = await createEvents(req);
-
-    res.status(StatusCodes.CREATED).json({
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
 
 const index = async (req, res, next) => {
   try {
     const result = await getAllEvents(req);
 
     res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const create = async (req, res, next) => {
+  try {
+    const result = await createEvents(req);
+
+    res.status(StatusCodes.CREATED).json({
       data: result,
     });
   } catch (err) {
@@ -68,10 +70,24 @@ const destroy = async (req, res, next) => {
   }
 };
 
+const changeStatus = async (req, res, next) => {
+  try {
+    const result = await changeStatusEvents(req);
+
+    res.status(StatusCodes.OK).json({
+      data: result,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+
 module.exports = {
   index,
   find,
   update,
   destroy,
   create,
+  changeStatus
 };
--- app/services/mongoose/events.js
+++ app/services/mongoose/events.js
@@ -8,7 +8,7 @@ const { checkingTalents } = require("./talents");
 const { NotFoundError, BadRequestError } = require("../../errors");
 
 const getAllEvents = async (req) => {
-  const { keyword, category, talent } = req.query;
+  const { keyword, category, talent, status } = req.query;
   let condition = { organizer: req.user.organizer };
 
   if (keyword) {
@@ -23,6 +23,10 @@ const getAllEvents = async (req) => {
     condition = { ...condition, talent: talent };
   }
 
+  if (["Draft", "Published"].includes(status)) {
+    condition = { ...condition, statusEvent: status };
+  }
+
   const result = await Events.find(condition)
     .populate({ path: "image", select: "_id name" })
     .populate({
@@ -35,6 +39,7 @@ const getAllEvents = async (req) => {
       populate: { path: "image", select: "_id  name" },
     });
 
+  console.log(result);
   return result;
 };
 
@@ -178,10 +183,36 @@ const deleteEvents = async (req) => {
   return result;
 };
 
+const changeStatusEvents = async (req) => {
+  const { id } = req.params;
+  const { statusEvent } = req.body;
+
+  if (!["Draft", "Published"].includes(statusEvent)) {
+    throw new BadRequestError("Status harus Draft atau Published");
+  }
+
+  // cari event berdasarkan field id
+  const checkEvent = await Events.findOne({
+    _id: id,
+    organizer: req.user.organizer,
+  });
+
+  // jika id result false / null maka akan menampilkan error `Tidak ada acara dengan id` yang dikirim client
+  if (!checkEvent)
+    throw new NotFoundError(`Tidak ada acara dengan id :  ${id}`);
+
+  checkEvent.statusEvent = statusEvent;
+
+  await checkEvent.save();
+
+  return checkEvent;
+};
+
 module.exports = {
   getAllEvents,
   createEvents,
   getOneEvents,
   updateEvents,
   deleteEvents,
+  changeStatusEvents
 };
